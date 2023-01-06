4d4392c3d87181ef89db7539895f0e26d83a7a2b
Konstantin Scheglov <scheglov@google.com>
2023-01-05 18:04:56 -0800

    Add test suite. (#3)

---
+++ src/test/java/com/fedor/cs34/discord/bot/BotTestSuite.java
@@ -0,0 +1,9 @@
+package com.fedor.cs34.discord.bot;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("com.fedor.cs34.discord.bot")
+public class BotTestSuite {
+}
