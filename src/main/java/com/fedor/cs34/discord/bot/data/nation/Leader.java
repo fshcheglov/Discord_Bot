cc338e6def3a9ffd1502ef4a11465d7a039f5253
fshcheglov <30306614+fshcheglov@users.noreply.github.com>
2023-01-05 18:18:44 -0800

    Test leader (#4)
    * Test for GovernmentDAO.
    
    * Added tests for LeaderDAO.
    
    * Add test suite. (#3)
    
    Co-authored-by: Konstantin Scheglov <scheglov@google.com>

--- src/main/java/com/fedor/cs34/discord/bot/data/nation/Leader.java
+++ src/main/java/com/fedor/cs34/discord/bot/data/nation/Leader.java
@@ -1,11 +1,23 @@
 package com.fedor.cs34.discord.bot.data.nation;
 
 public class Leader {
-    public String name;
-    public int id;
+    private int id;
+    private final String name;
 
-    public Leader(String name, int id) {
+    public Leader(int id, String name) {
+        this.id = id;
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
         this.id = id;
     }
 }
